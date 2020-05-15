82ded9f1526a2a447df6d39206e46e8b26bb4670
jordseow <jordan_seow@hotmail.com>
2020-05-16 05:27:27 +0800

    Redesign landing

--- src/views/Landing/Register.vue
+++ src/views/Landing/Register.vue
@@ -55,48 +55,6 @@
                   />
                 </div>
               </div>
-              <div class="form-group row">
-                <label for="faculty" class="col-md-4 col-form-label text-md-right">Faculty</label>
-
-                <div class="col-md-6">
-                  <input
-                    id="faculty"
-                    type="faculty"
-                    class="form-control"
-                    name="faculty"
-                    required
-                    v-model="form.faculty"
-                  />
-                </div>
-              </div>
-              <div class="form-group row">
-                <label for="course" class="col-md-4 col-form-label text-md-right">Course</label>
-
-                <div class="col-md-6">
-                  <input
-                    id="course"
-                    type="course"
-                    class="form-control"
-                    name="course"
-                    required
-                    v-model="form.course"
-                  />
-                </div>
-              </div>
-              <div class="form-group row">
-                <label for="year" class="col-md-4 col-form-label text-md-right">Year</label>
-
-                <div class="col-md-6">
-                  <input
-                    id="year"
-                    type="year"
-                    class="form-control"
-                    name="year"
-                    required
-                    v-model="form.year"
-                  />
-                </div>
-              </div>
               <div class="form-group row mb-0">
                 <div class="col-md-8 offset-md-4">
                   <button type="submit" class="btn btn-primary">Register</button>
@@ -120,9 +78,6 @@ export default {
         name: "",
         email: "",
         password: "",
-        course: "",
-        year: "",
-        faculty: ""
       },
       error: null
     };
@@ -138,14 +93,8 @@ export default {
             .doc(cred.user.uid)
             .set({
               username: this.form.name,
-              tutor: false,
               email: this.form.email,
               password: this.form.password,
-              course: this.form.course,
-              year: this.form.year,
-              faculty: this.form.faculty,
-              numstudent: 0,
-              rate: 0,
             });
 
           this.$router.push('home')
