34fc5e83e1c54be45fd4a53feb7aefd21373b07f
weigenie <ieeweigen@hotmail.com>
2020-05-17 00:05:38 +0800

    cleaned up code for scanneritem and added comments

--- src/components/ScannerItem.vue
+++ src/components/ScannerItem.vue
@@ -8,14 +8,18 @@
             :class="`elevation-${hover ? 12 : 2}`"
             class="profile"
           >
+
+          <!-- company logo -->
             <div v-if="isCompared" class="top-card1" style="height:6.5em">
               <v-avatar size="120" class="avatar">
-                <img src="razerlogo.jpg" class="image" />
+                  <!-- placeholder image -->
+                <img src="razerlogo.jpg" class="image" /> 
               </v-avatar>
             </div>
 
             <div v-else class="top-card" style="height:6.5em">
               <v-avatar size="120" class="avatar">
+                  <!-- placeholder image -->
                 <img src="razerlogo.jpg" class="image" />
               </v-avatar>
             </div>
@@ -75,15 +79,13 @@
                 </b-modal>
                 </div>
 
+                <!-- company brief information -->
                 </v-col>
                 <h3>{{ service.company }}</h3>
                 <div>
                   <h6>{{ service.desc }}</h6>
                   <body>
                     Category: {{ service.category }} <br />
-                    <!-- Contracted: {{ service.contracted_count }}<br />
-                    Price: ${{ service.price }} <br />
-                    Rating: {{ calculateAverage }}/5 <br /> -->
                     <v-btn @click="order(service)" color="#66b933" style="text">
                     Consult
                     </v-btn>
@@ -123,15 +125,9 @@
 </template>
 
 <script>
-import StarRating from "vue-star-rating";
 import firebase from "firebase";
-import VueSimpleAlert from "vue-simple-alert";
 
 export default {
-  components: {
-    StarRating,
-    VueSimpleAlert,
-  },
   props: [
     "order",
     "service",
@@ -144,67 +140,9 @@ export default {
   data() {
     return {
         modalShow: false,
-      data: {
-        hover: false,
-        category: "",
-        company: "",
-        contracted_count: 0,
-        description: "",
-        price: 0,
-        total_rating_count: 0,
-        total_rating: 0,
-        average_rating: 0,
-      },
     };
   },
-  methods: {
-    showInfo() {
-      this.$fire({title: this.service.company, text: this.service.company});
-    },
-  },
-  //   fetchUser() {
-  //     firebase.auth().onAuthStateChanged(user => {
-  //       if (user) {
-  //         // User is signed in.
-  //         var db = firebase.firestore();
-  //         var docRef = db.collection("services");
-  //         docRef
-  //           .get()
-  //           .then(doc => {
-  //             if (doc && doc.exists) {
-  //               const myData = doc.data();
-  //               this.data.category = myData.category;
-  //               this.data.company = myData.company;
-  //               this.data.contracted_count = myData.contracted_count;
-  //               this.data.description = myData.description;
-  //               this.data.price = myData.price;
-  //               this.data.total_rating_counts = myData.total_ratings_counts;
-  //               this.data.total_ratings = myData.total_ratings;
-
-  //               if (total_rating_counts == 0) {
-  //                   this.average_ratings = 0;
-  //               } else {
-  //                   this.average_ratings = this.total_ratings / this.total_rating_counts;
-  //               }
-  //             }
-  //           })
-  //           .catch(error => {
-  //             console.log("Got an error: ", error);
-  //           });
-  //       } else {
-  //         console.log("not signed in");
-  //       }
-  //     });
-  //   }
-  // },
   computed: {
-    calculateAverage() {
-      return this.service.total_rating_count == 0
-        ? 0
-        : (this.service.total_rating / this.service.total_rating_count).toFixed(
-            2
-          );
-    },
     isCompared() {
       // check whether in compared
       if (this.comparedItems.includes(this.service)) {
@@ -212,29 +150,6 @@ export default {
       } else {
         return false;
       }
-    },
-    profileImg() {
-      firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-          // User is signed in.
-          var db = firebase.firestore();
-          var docRef = db.collection("users").doc(user.uid);
-          docRef
-            .get()
-            .then((doc) => {
-              if (doc && doc.exists) {
-                this.image = doc.data().image;
-                console.log("file", this.image);
-                return require(`../assets/${this.image}`);
-              }
-            })
-            .catch((error) => {
-              console.log("Got an error: ", error);
-            });
-        } else {
-          console.log("not signed in");
-        }
-      });
     },
   },
 };
